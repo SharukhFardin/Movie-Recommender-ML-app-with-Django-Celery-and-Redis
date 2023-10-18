6114b22d4d3464dbc293049606be70f1d893ce69
Sharukh Fardin <sharukhfardin1234@gmail.com>
2023-10-18 22:40:11 +0600

    Few changes added.

--- django/recommender/utils.py
+++ django/recommender/utils.py
@@ -1,11 +1,18 @@
 import csv
+import datetime
 
+from pprint import pprint
 from django.conf import settings
 
 from faker import Faker
 
-# DATA_DIR = seetiin
+MOVIE_METADATA_CSV = settings.DATA_DIR / "movies_metadata.csv"
 
+
+def load_movie_data(limit=10):
+    with open(MOVIE_METADATA_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for i, row in enumerate(reader)
 
 def get_fake_profiles(count=10):
     # Generates fake profile data
